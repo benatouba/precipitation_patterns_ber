7079e80aa6caba50237dc71c2c0c2d127ebca092
Benjamin Schmidt <smidbenq@master.cluster>
2018-10-10 12:51:20 +0200

    Set up external PATH/ENV_VARIABLE reading.

--- copy_era5.sh
+++ copy_era5.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
 # PATHS AND FILES
-DEST_PATH='/data/scratch/smidbenq/era5/'
-SOURCE_PATH='/data/projects/ensembles/era5/analysis/cclm_forcing/year2013/'
-cd ${DEST_PATH}
-touch out.txt
+# export file paths from locally stored file
+. ./file_paths.sh
+
+echo ${ERA5_SCRT}
+cd ${ERA5_SCRT}
+OUT=${OUT}/copy_era5.txt
 
 # OPTIONS
 COMPR=0
 
 # RECORD TIME
-echo -e "$(date -u): Job started!\n" > out.txt
+echo -e "$(date -u): Job started!\n" > ${OUT}
 STARTTIME=$(date -u +%s)
 
 # PROGRAM
-for i in ${SOURCE_PATH}/caf20130[5-8]*; do
+for i in ${ERA5_FRC}/caf20130[5-8]*; do
     SECONDS=0
 
     echo -e "$(date -u):\nnccopy\n${i}\nto\n\
-${DEST_PATH}$(basename -s .nc ${i}).nc\n" >> out.txt
+${ERA5_SCRT}$(basename -s .nc ${i}).nc\n" >> ${OUT}
 
     nccopy -d ${COMPR} ${i} $(basename -s .nc ${i}).nc
 
     echo -e "$(date -u):\nDone with file\n\
-$(basename -s .nc ${i}).nc\nin\ntime = ${SECONDS} seconds!\n" >> out.txt
+$(basename -s .nc ${i}).nc\nin\ntime = ${SECONDS} seconds!\n" >> ${OUT}
 done
 
 # RECORD ENDTIME
 ENDTIME=$(date -u +%s)
-echo -e "$(date -u): Finished Job in $((${ENDTIME} - ${STARTTIME})) seconds!" >> out.txt
+echo -e "$(date -u): Finished Job in $((${ENDTIME} - ${STARTTIME})) seconds!" >> ${OUT}
